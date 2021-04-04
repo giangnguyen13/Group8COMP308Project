50f916c2de483ffa7c19694fc76773f25d97008f
dlee-171 <dlee171@my.centennialcollege.ca>
2021-04-04 19:30:26 -0400

    working- machine learning

--- app/controllers/patient.server.controller.js
+++ app/controllers/patient.server.controller.js
@@ -131,3 +131,184 @@ exports.showVideo = function (req, res) {
   console.log(video.title);
   res.json(video);
 };
+
+
+
+var fs = require('fs');
+var csv = require('csv');
+var C45 = require('c4.5');
+
+exports.trainAndPredict = function (req, res) {
+  
+  console.log("in trainAndPredict?");
+  fs.readFile('trainingset_half.csv', function(err, data) {
+    if (err) {
+      console.log("err1");
+      console.error(err);
+      return false;
+    }
+   
+    csv.parse(data, function(err, data) {
+      if (err) {
+        console.log("err2");
+        console.error(err);
+        return false;
+      }
+   
+      var headers = data[0];
+      var features = headers.slice(1,-1); // 
+      console.log('feature.length:',features.length);
+      var featureTypes = Array(133).fill('category');
+     
+      var trainingData = data.slice(1).map(function(d) {
+        return d.slice(1);
+      });
+      var target = headers[headers.length-1]; // "disease"
+      var c45 = C45();
+   
+      c45.train({
+          data: trainingData,
+          target: target,
+          features: features,
+          featureTypes: featureTypes
+      }, function(error, model) {
+        if (error) {
+          console.log('error3');
+          console.error(error);
+          return false;
+        }
+   
+        var testData = [
+           
+          ['1','0','0','0','0','0','0','0','0','0','0','0','0',
+          '0','1','0','0','0','0','0','0','1','0','0','0','0','0',
+          '0','0','0','0','0','1','1','0','1','0','0','0','1','0','0',
+          '1','1','0','0','0','0','1','0','0','0','0','0','0','0','0',
+          '0','0','0','0','0','0','0','0','0','0','0','0','0','0','0',
+          '0','0','0','0','0','0','0','0','0','0','0','0','0','0','0',
+          '0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0',
+          '0','0','0','0','0','0','0','0','1','1','0','0','0','0','0','0',
+          '0','0','0','0','0','0','0','0','0','0','0','0','0','0']
+
+        ];
+        
+        //console.log(testData[0].length);
+        var model = c45.getModel();
+        console.log(model.classify(testData[0]));
+
+      });
+    });
+  });
+}
+
+exports.trainAndPredict1= function(req,res){
+  fs.readFile('data.csv', function(err, data) {
+    if (err) {
+      console.error(err);
+      return false;
+    }
+   
+    csv.parse(data, function(err, data) {
+      if (err) {
+        console.error(err);
+        return false;
+      }
+   
+      var headers = data[0];
+      var features = headers.slice(1,-1); // ["attr1", "attr2", "attr3"]
+      var featureTypes = ['category','number','category'];
+      var trainingData = data.slice(1).map(function(d) {
+        return d.slice(1);
+      });
+      var target = headers[headers.length-1]; // "class"
+      console.log(target)
+      console.log(features)
+      var c45 = C45();
+   
+      c45.train({
+          data: trainingData,
+          target: target,
+          features: features,
+          featureTypes: featureTypes
+      }, function(error, model) {
+        if (error) {
+          console.error(error);
+          return false;
+        }
+   
+        var testData = [
+          ['B',71,'False'],
+          ['C',70,'True'],
+        ];
+   
+        console.log(model.classify(testData[0]) === 'CLASS1'); // true
+        console.log(model.classify(testData[1]) === 'CLASS2'); // true
+      });
+    });
+  });
+}
+
+var DecisionTree = require('decision-tree');
+exports.trainAndPredict2 = function (req, res) {
+  fs.readFile('trainingset_test.csv', function(err, data) {
+    if (err) {
+      console.log("err1");
+      console.error(err);
+      return false;
+    }
+   
+    csv.parse(data, function(err, data) {
+      if (err) {
+        console.log("err2");
+        console.error(err);
+        return false;
+      }
+   
+      var headers = data[0];
+      var features = headers.slice(0,-1); // 
+      console.log(features);
+
+     
+      var training_data = data.slice(1).map(function(d) {
+        return d.slice(1);
+      });
+      console.log(training_data);
+
+        var testData = 
+          [['0','1','1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1']]
+        ;
+      var class_name = "Disease";
+
+      console.log(testData);
+      var dt = new DecisionTree(training_data, class_name, features);
+      var predicted_class = dt.predict(testData);
+      var accuracy = dt.evaluate(testData);
+      console.log(predicted_class);
+      console.log(accuracy);
+      
+
+      
+    });
+  });
+
+
+  // var training_data = [
+  // 	{"color":"blue", "shape":"square", "liked":false},
+  // 	{"color":"red", "shape":"square", "liked":false},
+  // 	{"color":"blue", "shape":"circle", "liked":true},
+  // 	{"color":"red", "shape":"circle", "liked":true},
+  // 	{"color":"blue", "shape":"hexagon", "liked":false},
+  // 	{"color":"red", "shape":"hexagon", "liked":false},
+  // 	{"color":"yellow", "shape":"hexagon", "liked":true},
+  // 	{"color":"yellow", "shape":"circle", "liked":true}
+  // ];
+
+  // var test_data = [
+  // 	{"color":"blue", "shape":"hexagon", "liked":false},
+  // 	{"color":"red", "shape":"hexagon", "liked":false},
+  // 	{"color":"yellow", "shape":"hexagon", "liked":true},
+  // 	{"color":"yellow", "shape":"circle", "liked":true}
+  // ];
+
+ 
+}
--- app/routes/app.server.routes.js
+++ app/routes/app.server.routes.js
@@ -33,4 +33,7 @@ module.exports = function (app) {
         .get(patientController.showVideo);
 
     app.param("videoId", patientController.videoById);
+
+    app.get("/run", patientController.trainAndPredict);
+
 };
