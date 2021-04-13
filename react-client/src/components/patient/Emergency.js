274d03f7f9a6a6f511022260d60a5bfa0f788fa0
johnny <superstar41668087@gmail.com>
2021-04-12 21:50:33 -0400

    added nurse List

--- react-client/src/components/patient/Emergency.js
+++ react-client/src/components/patient/Emergency.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { isUserAuthenticated } from "../../Helper";
 import { Redirect } from "react-router-dom";
+import ListGroup from "react-bootstrap/ListGroup";
+import Spinner from "react-bootstrap/esm/Spinner";
+import axios from "axios";
 
 function Emergency() {
+  const nurseApiUrl = "http://localhost:5000/api/nurses";
+
+  const [nurseList, setNurseList] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchNurseList();
+  }, []);
+
+  const fetchNurseList = () => {
+    axios
+      .get(nurseApiUrl)
+      .then((res) => {
+        setNurseList(res.data);
+        console.log(res);
+        console.log(nurseList);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  if (!isUserAuthenticated()) {
+    return <Redirect to="/patient" />;
+  }
   return (
     <>
-      {isUserAuthenticated() ? (
-        <div className="App">This is Emergency route for patient</div>
+      {isLoading ? (
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
       ) : (
-        <Redirect to="/patient" />
+        <ListGroup>
+          {nurseList.map((nurse, idx) => (
+            <ListGroup.Item key={idx} action onClick={() => {}}>
+              {nurse.fullName}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
       )}
     </>
   );
