0dd9c5f8a62acb8a1d15bbcc033ce9d24e52bcc6
Giang Nguyen <giangkawaii1999@gmail.com>
2021-04-17 11:11:08 -0400

    fix app ui

--- react-client/src/components/IndexScreen.js
+++ react-client/src/components/IndexScreen.js
@@ -3,7 +3,17 @@ import React from 'react';
 function IndexScreen() {
     return (
         <div className='App'>
-            <h1>App route</h1>
+            <h1>
+                Hello, Welcome to the Health Vital Sign Tracking Application
+            </h1>
+            <h2>Choose your role</h2>
+            <a href='/patient/login' className='btn btn-lg btn-success'>
+                I'm patient
+            </a>
+            &nbsp;
+            <a href='/nurse/login' className='btn btn-lg btn-primary'>
+                I'm nurse
+            </a>
         </div>
     );
 }
